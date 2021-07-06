017e5941dc3f5f54d24ca5c66e77efe2536090bf
fanita90 <parroquini_90@hotmail.com>
2021-07-05 19:23:46 -0500

    Primer commit.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
+const codifica = () => {
+    let string = document.getElementById("string").value;
+    let offset = document.getElementById("offset").value;
+    cipher.encode(string, offset);
+}
+
 console.log(cipher);
