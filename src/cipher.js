1dfa8c38a59a036c77799991b9fb499ca2816a25
Fanita90 <parroquini_90@hotmail.com>
2021-07-08 19:03:38 -0500

    Cifrado funcional,css implementado

--- src/cipher.js
+++ src/cipher.js
@@ -1,41 +1,75 @@
 const cipher = {
-  //let string = "  mi menSaje secReto";
 
-  //let offset = 3;
+  encode: function (offset, string) {
+    string = string.trim();//
 
-
-  enconde: function (string, offset) {
-    string = string.trim();
-    let result = "";
-    let newValor;
+    let result = ""; //Variable que guardará resultado
+    let newValor = 0;
     //for: recorre nuestra cadena y aplicamos charCodeAt para que nos regrese el valor ascii en la variable stringAscii
-    for (i = 0; i < string.length; i++) {
+    for (let i = 0; i < string.length; i++) {
       let stringAscii = string.charCodeAt(i);
       //si nuestra variable stringAscii se encuentra en el rango 65-91 entonces es mayúscula y aplicamos la formula
       if ((stringAscii >= 65) && (stringAscii < 91)) {
-        newValor = ((stringAscii - 65 + offset) % 26 + 65);
+        newValor = ((stringAscii - 65 + offset) % 26) + 65;
       }
-      /*si nuestra variable stringAscii se encuentra en el rango de 97-123 entonces es minúscula y 
+      /*si nuestra variable stringAscii se e 
+      ncuentra en el rango de 97-123 entonces es minúscula y 
       aplicamos la siguiente fórmula, el valor de 65 cambia a 97 porque en ascii es donde empieza en abecedario en minúsculas,
       y termina en el 123*/
       else if ((stringAscii >= 97) && (stringAscii < 123)) {
-        newValor = ((stringAscii - 97 + offset) % 26 + 97);
+        newValor = ((stringAscii - 97 + offset) % 26) + 97;
       }
       /*si no es mayuscula o minúscula, entonces es un espacio, que en ascii se encuentra en la posición 32, 
       lo ponemos en módulo 1 porque no se desplazará y solo necesitamos el valor 32*/
       else {
-        newValor = ((stringAscii - 32 + offset) % 1 + 32);
+        newValor = ((stringAscii - 32 + offset) % 1) + 32;
       }
       /*Obtenemos el valor ascii de nuestra cadena y la desencriptamos*/
-      newString = String.fromCharCode(newValor);
+      let newString = String.fromCharCode(newValor);
       //concatenamos todos las letras nuevas y las guardamos en resultado.
       result += newString;
     }//cierre for
     //Retornamos el resultado de la cadena con las letras ya sustituidas
-    console.log(result);
-    return result;
-  }//cierre function encode
-  //encode(string, offset);
-};
 
-//export default cipher;
+    return result;
+  },
+
+
+
+
+
+  decode: function (offset, string) {
+    string = string.trim();
+
+    let result = ""; //Variable que guardará resultado
+    let newValor = 0;
+
+    for (let i = 0; i < string.length; i++) {
+      let stringAscii = string.charCodeAt(i);
+
+      if ((stringAscii >= 65) && (stringAscii < 91)) {
+        newValor = ((stringAscii - 65 + (26 - offset)) % 26) + 65;
+
+
+      }
+
+      else if ((stringAscii >= 97) && (stringAscii < 123)) {
+        newValor = ((stringAscii - 97 + (26 - offset)) % 26) + 97;
+
+      }
+
+      else {
+        newValor = ((stringAscii - 32 + (26 - offset)) % 1) + 32;
+
+      }
+
+      let newString = String.fromCharCode(newValor);
+
+
+      result += newString;
+    }
+    return result;
+  }
+
+};
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import cipher from './cipher.js';
 
-const codifica = () => {
+document.getElementById("cifrar").addEventListener("click", (event) => {
+    event.preventDefault();
     let string = document.getElementById("string").value;
-    let offset = document.getElementById("offset").value;
-    cipher.encode(string, offset);
-}
+    if (string == "") {
+        alert("Rellena el campo de texto");
+    }
 
-console.log(cipher);
+    else if (typeof string === Number) {
+        alert("Solo se aceptan letras");
+    }
+    let offset = document.getElementById("offset").value;
+    offset = parseInt(offset);
+
+    let resultado = cipher.encode(offset, string);
+    let mostrarResultado = document.getElementById("string")
+    mostrarResultado.value = resultado;
+})
+
+document.getElementById("descifrar").addEventListener("click", (event) => {
+    event.preventDefault();
+    let string = document.getElementById("string").value;
+
+    let offset = document.getElementById("offset").value;
+    offset = parseInt(offset);
+
+    let resultado = cipher.decode(offset, string);
+    let mostrarResultado = document.getElementById("string")
+    mostrarResultado.value = resultado;
+
+})
+
