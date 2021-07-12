a1326c08f5ce82dc4ab4769902d92199634bac25
Fanita90 <parroquini_90@hotmail.com>
2021-07-12 13:39:32 -0500

    cambios finales en toda la aplicación

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,40 @@
 const cipher = {
 
   encode: function (offset, string) {
-    string = string.trim();//
+    /*Eliminamos espacios en blanco antes o después de la cadena con método .trim*/
+    string = string.trim();
+    let result = "";// Variable que guardará resultado
+    let newValor = 0;//Guardará el código ascii de cada valor de la cadena si cumple con alguna condición.
 
-    let result = ""; //Variable que guardará resultado
-    let newValor = 0;
-    //for: recorre nuestra cadena y aplicamos charCodeAt para que nos regrese el valor ascii en la variable stringAscii
+    /*Recorremos cadena y aplicamos charCodeAt para que regrese valor ascii en variable.*/
     for (let i = 0; i < string.length; i++) {
       let stringAscii = string.charCodeAt(i);
-      //si nuestra variable stringAscii se encuentra en el rango 65-91 entonces es mayúscula y aplicamos la formula
+
+      /*Condicionamos variable dentro de un rango de >=65 && <91 para obtener MAYÚSCULAS*/
       if ((stringAscii >= 65) && (stringAscii < 91)) {
+        /*Si condición:true aplicamos fórmula*/
         newValor = ((stringAscii - 65 + offset) % 26) + 65;
       }
-      /*si nuestra variable stringAscii se e 
-      ncuentra en el rango de 97-123 entonces es minúscula y 
-      aplicamos la siguiente fórmula, el valor de 65 cambia a 97 porque en ascii es donde empieza en abecedario en minúsculas,
-      y termina en el 123*/
+      /*Condicionamos variable dentro de un rango de >=97 && <123 para obtener MINUSCULAS*/
       else if ((stringAscii >= 97) && (stringAscii < 123)) {
+        /*Si condición:true aplicamos fórmula*/
         newValor = ((stringAscii - 97 + offset) % 26) + 97;
       }
-      /*si no es mayuscula o minúscula, entonces es un espacio, que en ascii se encuentra en la posición 32, 
-      lo ponemos en módulo 1 porque no se desplazará y solo necesitamos el valor 32*/
+      /*Si ninguna de las condiciones se cumple,entonces aplicamos fórmula para ESPACIOS*/
       else {
         newValor = ((stringAscii - 32 + offset) % 1) + 32;
       }
-      /*Obtenemos el valor ascii de nuestra cadena y la desencriptamos*/
-      let newString = String.fromCharCode(newValor);
-      //concatenamos todos las letras nuevas y las guardamos en resultado.
-      result += newString;
-    }//cierre for
-    //Retornamos el resultado de la cadena con las letras ya sustituidas
 
+      /*a cada valor ascii de nuestra cadena, aplicamos string.fromCarcode para que nos retorne la letra que le corresponde*/
+      let newString = String.fromCharCode(newValor);
+
+      /*concatenamos todos las letras nuevas y las guardamos en resultado.*/
+      result += newString;
+
+    }
+    /*Retornamos el resultado de la cadena con las letras ya sustituidas*/
     return result;
   },
-
-
 
 
 
@@ -42,27 +42,27 @@ const cipher = {
     string = string.trim();
 
     let result = ""; //Variable que guardará resultado
-    let newValor = 0;
+    let newValor = 0;//Guardará el código ascii de cada valor de la cadena si cumple con alguna condición.
 
+    /*Recorremos cadena y aplicamos charCodeAt para que regrese valor ascii en variable.*/
     for (let i = 0; i < string.length; i++) {
       let stringAscii = string.charCodeAt(i);
 
+      /*Condicionamos variable dentro de un rango de >=65 && <91 para obtener MAYÚSCULAS*/
       if ((stringAscii >= 65) && (stringAscii < 91)) {
-        newValor = ((stringAscii - 65 + (26 - offset)) % 26) + 65;
-
-
+        newValor = (stringAscii + 65 - offset) % 26 + 65;
       }
-
+      /*Condicionamos variable dentro de un rango de >=97 && <123 para obtener MINUSCULAS*/
       else if ((stringAscii >= 97) && (stringAscii < 123)) {
-        newValor = ((stringAscii - 97 + (26 - offset)) % 26) + 97;
-
+        newValor = (stringAscii - 97 + (26 - offset)) % 26 + 97;
       }
 
+      /*Si ninguna de las condiciones se cumple,entonces aplicamos fórmula para ESPACIOS*/
       else {
-        newValor = ((stringAscii - 32 + (26 - offset)) % 1) + 32;
+        newValor = (stringAscii - 32 + (1 - offset) % 1) + 32;
 
       }
-
+      /*a cada valor ascii de nuestra cadena, aplicamos string.fromCarcode para que nos retorne la letra que le corresponde*/
       let newString = String.fromCharCode(newValor);
 
 
